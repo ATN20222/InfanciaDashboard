e4253cfa67a4fea2e9d8f58f4a2cf7c57d95d39b
ATN20222 <antonabdalla30@gmail.com>
2024-07-03 14:01:54 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import logo1 from '../src/INFANCIA_LOGO.png';
+import logo2 from '../src/InfanciaLogoText.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo1} className="App-logo" alt="logo" />
+        {/* <img src={logo2} className="App-logo" width="300px" height="20px" alt="logo" /> */}
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        
+          
+        
+        Infancia Dashboard
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
